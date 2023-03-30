59e3e084f2763fa9855a8a069c7789f85f31b6f5
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-03-30 21:35:34 +0300

    feat: create store slice

--- src/calculator/slice/calculator-slice.ts
+++ src/calculator/slice/calculator-slice.ts
@@ -1,8 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import { config, data } from '../data'
+
+export type ConfigType = {
+  type: string
+  key: string
+  name: string
+  min?: number
+  max?: number
+  step?: number
+  value?: number
+}
+
+export type DataType = {
+  type: string
+  name: string
+  material?: string
+  unit: string
+  width?: number
+  price: number
+}
+
+type InitialStateType = {
+  data: DataType[]
+  config: ConfigType[]
+}
+
+const initialState: InitialStateType = { data, config }
+
 export const calculatorSlice = createSlice({
   name: 'calculator',
-  initialState: {},
+  initialState,
   reducers: {
     calculate: state => state,
   },
